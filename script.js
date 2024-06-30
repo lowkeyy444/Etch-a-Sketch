1dd6e3bd5e9f0eaf09d3a99abec23ed8bf693461
lowkeyy444 <143279097+lowkeyy444@users.noreply.github.com>
2024-06-30 12:17:18 +0530

    created a user prompt feature which adds inputs the grid size

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
-const container = document.querySelector("#div-container");
-container.setAttribute("style", "width: 400px; height:400px");
-for (let i = 0; i < 256; i++) {
-  const div = document.createElement("div");
-  div.classList.add("div");
-  container.appendChild(div);
-
-  div.addEventListener("mouseover", () => {
-    div.style.backgroundColor = "blue";
-    console.log("test");
-  });
+function checkSize() {
+  let size = prompt("size?");
+  return size;
 }
+
+const btn = document.querySelector("#btn");
+
+btn.addEventListener("click", () => {
+  let gridSize = checkSize();
+  console.log(gridSize);
+
+  const container = document.querySelector("#div-container");
+  container.innerHTML = "";
+  container.setAttribute("style", "width: 600px; height:600px");
+
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const div = document.createElement("div");
+    div.classList.add("div");
+    div.setAttribute(
+      "style",
+      "height: " + 600 / gridSize + "px; width: " + 600 / gridSize + "px;"
+    );
+    container.appendChild(div);
+
+    div.addEventListener("mouseover", () => {
+      div.style.backgroundColor = "blue";
+      console.log("test");
+    });
+  }
+});
